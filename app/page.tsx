737910779ac4a488e6d76fd6f8c00e5d55b68c2f
Huntergs16 <samoyhun@msu.edu>
2023-06-12 13:27:33 -0500

    fixing build errors

--- app/page.tsx
+++ app/page.tsx
@@ -11,25 +11,44 @@ export default function Home() {
         <button className='w-max h-max  border border-solid'>Let's Connect</button>
       </header>
 
-      <section className='w-full flex flex-col gap-4'>
-        <h3 className='text-3xl text-center'><span className='font- font-bold'>Scalable &amp; Powerful</span> Wetware Computing</h3>
-        <p className="text-center">Developing wetware architecture for the next generation of computing and neuropharmacology.</p>
+      <section className="w-full flex flex-col gap-4">
+        <h3 className="text-3xl text-center">
+          <span className="font-bold">Scalable &amp; Powerful</span> Wetware
+          Computing
+        </h3>
+        <p className="text-center">
+          Developing wetware architecture for the next generation of computing
+          and neuropharmacology.
+        </p>
       </section>
 
       <section className="relative aspect-square w-24">
-        <Image src={"/pink-inference-icon.png"} fill alt="Pink Inference Logo" />
+        <Image
+          src={"/pink-inference-icon.png"}
+          fill
+          alt="Pink Inference Logo"
+        />
       </section>
 
-      <section className="rounded-[5rem] bg-clip-text bg-gradient-to-br from-[#f0f0f0] to-[#ffffff] shadow-lg w-full p-10">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quia facere explicabo error voluptatum quidem cupiditate magnam numquam praesentium sit, aut quas fuga rem non voluptates repudiandae ut illum culpa!
+      <section className="rounded-[5rem] bg-gradient-to-br from-gray-200 to-white shadow-lg w-full p-10">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quia
+        facere explicabo error voluptatum quidem cupiditate magnam numquam
+        praesentium sit, aut quas fuga rem non voluptates repudiandae ut illum
+        culpa!
       </section>
 
-      <section className="rounded-[5rem] bg-clip-text bg-gradient-to-br from-[#f0f0f0] to-[#ffffff] shadow-lg w-full p-10">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quia facere explicabo error voluptatum quidem cupiditate magnam numquam praesentium sit, aut quas fuga rem non voluptates repudiandae ut illum culpa!
+      <section className="rounded-[5rem] bg-gradient-to-br from-gray-200 to-white shadow-lg w-full p-10">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quia
+        facere explicabo error voluptatum quidem cupiditate magnam numquam
+        praesentium sit, aut quas fuga rem non voluptates repudiandae ut illum
+        culpa!
       </section>
 
-      <section className="rounded-[5rem] bg-clip-text bg-gradient-to-br from-[#f0f0f0] to-[#ffffff] shadow-lg w-full p-10">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quia facere explicabo error voluptatum quidem cupiditate magnam numquam praesentium sit, aut quas fuga rem non voluptates repudiandae ut illum culpa!
+      <section className="rounded-[5rem] bg-gradient-to-br from-gray-200 to-white shadow-lg w-full p-10">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quia
+        facere explicabo error voluptatum quidem cupiditate magnam numquam
+        praesentium sit, aut quas fuga rem non voluptates repudiandae ut illum
+        culpa!
       </section>
 
       <ParticlesWrapper />
