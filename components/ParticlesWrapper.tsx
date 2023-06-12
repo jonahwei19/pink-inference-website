2c1ca8290125b53d2c72c1a2ca79384872a4c418
Huntergs16 <samoyhun@msu.edu>
2023-06-12 15:37:59 -0500

    animation optimization for mobile devices

--- components/ParticlesWrapper.tsx
+++ components/ParticlesWrapper.tsx
@@ -1,101 +1,113 @@
 'use client'
 
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Particles from "react-tsparticles";
 import type { Container, Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 
 export default function ParticlesWrapper() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    // Initialize the tsParticles instance
+    await loadFull(engine);
+  }, []);
 
-    const particlesInit = useCallback(async (engine: Engine) => {
-      console.log(engine);
+  const particlesLoaded = useCallback(async (container: Container | undefined) => {
+    // tsParticles container loaded
+  }, []);
 
-      // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      await loadFull(engine);
-    }, []);
+  const [mobile, setMobile] = useState(false)
 
-    const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        await console.log(container);
-    }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.matchMedia("(max-width: 768px)").matches;
+      setMobile(!isMobile);
+    };
 
-    return (
-      <Particles
-          className="-z-10"
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            background: {
-              color: '#FFFFFF',
+    handleResize(); // Check on initial load
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Particles
+      className="-z-10"
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        // Default options
+        background: {
+          color: "#FFFFFF",
+        },
+        fpsLimit: mobile ? 60 : 120,
+        interactivity: {
+          detectsOn: "window",
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
             },
-            fpsLimit: 120,
-            interactivity: {
-              detectsOn: 'window',
-              events: {
-                onClick: {
-                  enable: false,
-                  mode: 'push',
-                },
-                onHover: {
-                  enable: true,
-                  mode: 'repulse', //repulse, attract, bubble, connect, grab, light, slow, trail
-                },
-              },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 100,
-                  duration: 5,
-                },
-              },
+            onHover: {
+              enable: mobile,
+              mode: "repulse",
             },
-            particles: {
-              color: {
-                value: '#e06666',
-              },
-              links: {
-                color: '#000000',
-                distance: 150,
-                enable: true,
-                opacity: 0.3,
-                width: 1,
-              },
-              collisions: {
-                enable: true,
-              },
-              move: {
-                direction: 'none',
-                enable: true,
-                outModes: {
-                  default: 'bounce',
-                },
-                random: false,
-                speed: 1,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 80,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: 'circle',
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
+          },
+          modes: {
+            push: {
+              quantity: 4,
             },
-            detectRetina: true,
-          }}
-      />
+            repulse: {
+              distance: 100,
+              duration: 5,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#e06666",
+          },
+          links: {
+            color: "#000000",
+            distance: 150,
+            enable: true,
+            opacity: 0.3,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
   );
 }
