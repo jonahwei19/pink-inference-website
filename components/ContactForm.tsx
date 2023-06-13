a489d227d6ac87c5f568912df4627b4aa41ad9a7
Huntergs16 <samoyhun@msu.edu>
2023-06-13 17:25:35 -0500

    updated sections to include placeholder info and faster particles

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -11,7 +11,7 @@ type FormValues = {
 
 const schema = z.object({
   name: z.string().nonempty('Name is required'),
-  email: z.string().email('Invalid email').nonempty('Email is required'),
+  email: z.string().nonempty('Email is required').email('Invalid email'),
   inquiry: z.string().nonempty('Inquiry is required'),
 });
 
@@ -30,23 +30,23 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <form className='w-full max-w-[600px] p-10 gap-4 bg-gradient-to-bl to-slate-700 from-slate-400 shadow-xl flex flex-col items-center rounded-xl' onSubmit={handleSubmit(onSubmit)}>
+    <form className='w-full max-w-[700px] bg-[#f0f0f0] p-10 gap-4 shadow-[inset_12px_64px_128px_12px_rgb(0,0,0,0.05)] flex flex-col items-center rounded-xl' onSubmit={handleSubmit(onSubmit)}>
       <div className='w-full max-w-[500px] min-w-[300px] flex flex-col'>
-        <label className='text-xl text-white' htmlFor="name">Name:</label>
-        <input className='bg-gradient-to-b from-slate-700 to-slate-500 rounded-xl px-1 py-2 shadow-inner text-white' type="text" id="name" {...register('name')} />
-        {errors.name && <span>{errors.name.message}</span>}
+        <label className='text-xl ' htmlFor="name">Name <span className='text-red-400'>*</span></label>
+        <input className='text-lg rounded-xl px-1 py-2 shadow-inner' type="text" id="name" {...register('name')} />
+        {errors.name && <span className='text-red-400'>{errors.name.message}</span>}
       </div>
       <div className='w-full max-w-[500px] min-w-[300px] flex flex-col'>
-        <label className='text-xl text-white' htmlFor="email">Email:</label>
-        <input className='bg-gradient-to-b from-slate-700 to-slate-500 rounded-xl px-1 py-2 shadow-inner text-white' type="text" id="email" {...register('email')} />
-        {errors.email && <span>{errors.email.message}</span>}
+        <label className='text-xl ' htmlFor="email">Email <span className='text-red-400'>*</span></label>
+        <input className='text-lg rounded-xl px-1 py-2 shadow-inner' type="text" id="email" {...register('email')} />
+        {errors.email && <span className='text-red-400'>{errors.email.message}</span>}
       </div>
       <div className='w-full max-w-[500px] min-w-[300px] flex flex-col'>
-        <label className='text-xl text-white' htmlFor="inquiry">Question/Inquiry:</label>
-        <textarea className='bg-gradient-to-b from-slate-700 to-slate-500 rounded-xl px-1 py-2 shadow-inner text-white' id="inquiry" {...register('inquiry')} />
-        {errors.inquiry && <span>{errors.inquiry.message}</span>}
+        <label className='text-xl ' htmlFor="inquiry">Question/Inquiry <span className='text-red-400'>*</span></label>
+        <textarea className='text-lg rounded-xl px-1 py-2 shadow-inner' id="inquiry" {...register('inquiry')} />
+        {errors.inquiry && <span className='text-red-400'>{errors.inquiry.message}</span>}
       </div>
-      <button className='hover:opacity-80 w-max px-4 py-2 text-black bg-gradient-to-br from-[#f0f0f0] to-[#ffffff] shadow-lg rounded-t-2xl rounded-b-2xl rounded-l-sm rounded-r-sm' type="submit">Submit</button>
+      <button className='hover:opacity-80 w-max px-4 py-2 bg-[#3772FF] text-[#f0f0f0] shadow-lg rounded-t-2xl rounded-b-2xl rounded-l-sm rounded-r-sm' type="submit">Submit</button>
     </form>
   );
 };
