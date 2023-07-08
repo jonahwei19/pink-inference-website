d6657edd2f1c7bd75da4710d0575876973273c24
Huntergs16 <samoyhun@msu.edu>
2023-07-08 12:07:35 -0500

    changed brain study to neural research

--- app/page.tsx
+++ app/page.tsx
@@ -243,7 +243,7 @@ export default function Home() {
                     alt='brain study'
                   />
                 </div>
-                <p>Brain Study</p>
+                <p>Neural Research</p>
               </div>
             </div>
 
