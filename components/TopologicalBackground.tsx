aa8629409300dc356db46915ed50b6dce9ad972c
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-23 12:53:04 -0400

    Update TopologicalBackground.tsx

--- components/TopologicalBackground.tsx
+++ components/TopologicalBackground.tsx
@@ -4,6 +4,10 @@ export default function TopologicalBackground() {
   return (
     <div className='absolute w-full h-full top-0 left-0 z-50 bg-blue-100'>
       <Script
+        src='/animations/topological/assets/glslcanvas.min.js'
+        strategy='beforeInteractive'
+      />
+      <Script
         src='/animations/topological/includes.js'
         strategy='beforeInteractive'
       />
@@ -15,10 +19,7 @@ export default function TopologicalBackground() {
         src='/animations/topological/frag.js'
         strategy='beforeInteractive'
       />
-      <Script
-        src='/animations/topological/assets/glslcanvas.min.js'
-        strategy='beforeInteractive'
-      />
+      <canvas id="yourCanvasId" />
       {/* Rest of your component */}
     </div>
   );
