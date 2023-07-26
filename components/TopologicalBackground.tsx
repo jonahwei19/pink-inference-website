cdedbacb9c071f1d8becd54d41a7573739812be7
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-26 00:24:45 -0400

    Update TopologicalBackground.tsx

--- components/TopologicalBackground.tsx
+++ components/TopologicalBackground.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
-import startAnimation from '../scripts/canvas.js';
-import { frag } from '../scripts/frag.js';
 
 export default function TopologicalBackground() {
   useEffect(() => {
-    startAnimation(frag);
+    import('../scripts/canvas.js').then(({ default: startAnimation }) => {
+      startAnimation();
+    });
   }, []);
 
   return (
-    <div id="hero" className='absolute w-full h-full top-0 left-0 z-50 bg-blue-100'>
-      {/* You can add more elements or components here if needed */}
+    <div className='absolute w-full h-full top-0 left-0 z-50 bg-blue-100'>
+      <script src='/animations/topological/assets/glslcanvas.min.js' />
+      {/* Rest of your component */}
     </div>
   );
 }
