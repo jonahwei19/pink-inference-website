8cb2e317e40a6101eddf89038b71ef7a0bc80580
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-26 00:45:58 -0400

    Update frag.js

--- scripts/frag.js
+++ scripts/frag.js
@@ -1,4 +1,3 @@
-module.exports.frag = `
 #ifdef GL_ES
 precision highp float;
 #endif
@@ -10,6 +9,24 @@ uniform vec2 u_mouse;
 uniform float seed;
 
 varying vec2 v_texcoord;
+
+// Placeholder implementation for random function (rand)
+float rand(vec2 co) {
+  // You can implement a random function here or use GLSL built-in functions
+  return 0.0;
+}
+
+// Placeholder implementation for 2D rotation function (rotation2d)
+vec2 rotation2d(float angle) {
+  // You can implement a 2D rotation function here or use GLSL built-in functions
+  return vec2(0.0);
+}
+
+// Placeholder implementation for fractal Brownian motion function (fbm)
+float fbm(vec2 uv) {
+  // You can implement the fbm function here or use GLSL noise functions
+  return 0.0;
+}
 
 void main(void)
 {
@@ -27,14 +44,13 @@ void main(void)
     f *= 50.0;
     f += u_time * 0.2;
     f += grain*3.0;
-    f += pow(mouse_distance,0.10);
+    f += pow(mouse_distance, 0.10);
     f = fract(f);
     float mixer = step(0.8, f) - step(0.9, f);
     
     vec4 black = vec4(0., 0., 0., 0.);
-    vec4 pink = vec4(254./255.,93./255.,168./255.,0.5);
+    vec4 pink = vec4(254./255., 93./255., 168./255., 0.5);
     vec4 color = mix(black, pink, mixer);
     
     gl_FragColor = color;
 }
-`;
