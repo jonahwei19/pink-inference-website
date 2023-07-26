a077331e8f3a175d9dbc5d40d26d433ab021b53c
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-26 00:40:05 -0400

    Update TopologicalBackground.tsx

--- components/TopologicalBackground.tsx
+++ components/TopologicalBackground.tsx
@@ -2,13 +2,19 @@ import { useEffect } from 'react';
 
 export default function TopologicalBackground() {
   useEffect(() => {
-    import('../scripts/canvas.js').then(({ default: startAnimation }) => {
-      startAnimation();
-    });
+    const startAnimation = require('../scripts/canvas.js');
+
+    startAnimation('../scripts/frag.js')
+      .then(() => {
+        console.log('Animation started successfully.');
+      })
+      .catch((error) => {
+        console.error('Failed to start animation:', error);
+      });
   }, []);
 
   return (
-    <div id="hero" className='absolute w-full h-full top-0 left-0 z-50 bg-blue-100'>
+    <div className='absolute w-full h-full top-0 left-0 z-50 bg-blue-100'>
       {/* Rest of your component */}
     </div>
   );
