8a59adf1c1281722a5ad8bcfa434265702516ad2
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-25 23:43:09 -0400

    Update TopologicalBackground.tsx

--- components/TopologicalBackground.tsx
+++ components/TopologicalBackground.tsx
@@ -1,16 +1,23 @@
 import Script from "next/script";
+import { useEffect } from 'react';
+import startAnimation from '/path/to/canvas.js';
+import { frag } from '/path/to/frag.js';
 
 export default function TopologicalBackground() {
+  useEffect(() => {
+    startAnimation(frag);
+  }, []);
+
   return (
     <div className='absolute w-full h-full top-0 left-0 z-50 bg-blue-100'>
       <Script
         src='/animations/topological/includes.js'
         strategy='beforeInteractive'
       />
-<Script src='/animations/topological/includes.js' strategy='beforeInteractive' />
-<Script src='/animations/topological/assets/glslcanvas.min.js' strategy='beforeInteractive' />
-<Script src='/animations/topological/canvas.js' strategy='beforeInteractive' />
-<Script src='/animations/topological/frag.js' strategy='beforeInteractive' />
+      <Script
+        src='/animations/topological/assets/glslcanvas.min.js'
+        strategy='beforeInteractive'
+      />
       {/* Rest of your component */}
     </div>
   );
