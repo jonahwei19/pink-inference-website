28d9f3768d3636de3dae0fe5f0cd8bb868611833
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-09-27 16:24:57 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,17 +29,8 @@ export default function Home() {
           <h1
             className={`text-white font-thin text-2xl ${robotoMonoFont.className}`}
           >
-            <span className='text-[#FE5DA8]'>PINK</span> INFERENCE
-          </h1>
-        </Link>
 
         <div className='relative w-[90vw] md:w-[70vw] lg:w-[60vw] xl:w-[50vw] 3xl:w-[45vw] aspect-square sm:-top-36 md:-top-52 left-[40%] lg:left-[50%] xl:left-[60%]'>
-          <Image
-            className='overflow-visible object-cover'
-            fill
-            src={"/logo.svg"}
-            alt='logo'
-          />
         </div>
 
         <div className='w-full relative -top-8 sm:top-5 md:-top-16 md:-bottom-1/2 flex flex-col gap-2 sm:gap-8 font-thin text-white break-words'>
@@ -47,10 +38,7 @@ export default function Home() {
             className={`text-4xl sm:text-5xl md:text-6xl lg:text-7xl ${robotoMonoFont.className} font-thin `}
           >
             <p data-aos='fade-up'>
-              SCALABLE <br />& POWERFUL
-            </p>
-            <p data-aos='fade-up' data-aos-delay={1200}>
-              WETWARE COMPUTING
+              ALRT
             </p>
           </h1>
           <div
@@ -61,29 +49,10 @@ export default function Home() {
             <p
               className={`text-sm sm:text-base md:text-2xl ${montserratFont.className} font-light lg:max-w-[60vw]`}
             >
-              Wetware architecture for the next generation of robotics
             </p>
             <div
               className={`text-sm sm:text-base md:text-2xl flex gap-4 sm:gap-12 flex-wrap lg:max-w-[60vw] ${robotoMonoFont.className} font-thin`}
             >
-              <Link
-                href={"/About"}
-                className='border-solid border-white border-spacing-3 border px-4 sm:px-7 py-2 rounded-md hover:opacity-70'
-              >
-                ABOUT
-              </Link>
-              <Link
-                href='/TeamSection'
-                className='border-solid border-white border-spacing-3 border px-4 sm:px-7 py-2 rounded-md hover:opacity-70'
-              >
-                TEAM
-              </Link>
-              <Link
-                href={"/Investors"}
-                className=' border-solid border-white border-spacing-3 border px-4 sm:px-7 py-2 rounded-md hover:opacity-70'
-              >
-                INVESTORS
-              </Link>
             </div>
           </div>
         </div>
@@ -120,49 +89,6 @@ export default function Home() {
         </div>
 
         <div className='w-screen h-8 bg-cover bg-center bg-topological-2' />
-
-        <div className='flex flex-col w-full px-[15%] sm:px-[25%]'>
-          <div className='flex flex-col gap-8'>
-            <h1
-              className={`font-thin text-4xl ${robotoMonoFont.className} font-thin`}
-            >
-              Why wetware?
-            </h1>
-            <p
-              className={`font-light text-sm ${montserratFont.className} font-light`}
-            >
-              It is no secret that neurons are notoriously difficult to keep
-              alive, let alone even obtain. So why would one bother with all the
-              hassle?
-            </p>
-            <p
-              className={`font-light text-sm ${montserratFont.className} font-light`}
-            >
-              One compelling reason is that biological neurons are advantageous
-              in both energy and data efficiency compared to traditional silicon
-              systems. More, biological neuronal networks exhibit massively
-              parallel computing power, high thermodynamic efficiency, and
-              real-time learning in-vitro. Wetware presents itself as a
-              potential solution to general intelligence problems and an
-              interesting new way to research age-old questions of cognition and
-              brain mechanisms.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <footer
-        className={`flex flex-col gap-2 justify-around items-center my-8 ${robotoMonoFont.className} font-light`}
-      >
-        <Image
-          src={"/logo.svg"}
-          width={48}
-          height={48}
-          alt='Pink Inference Logo'
-        />
-        <p>&copy; 2023 Pink Inference</p>
-        <p>Designed and Built by Hunter Samoy</p>
-      </footer>
     </main>
   );
 }
