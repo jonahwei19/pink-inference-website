16d9f9ff6929e2b95964af455dc41e660606b048
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-09-27 21:20:36 -0400

    Update frag.js

--- app/frag.js
+++ app/frag.js
@@ -73,7 +73,7 @@ void main(void)
     float mixer = step(0.8, f) - step(0.9, f);
     
     vec4 black = vec4(0., 0., 0., 0.);
-    vec4 orange = vec4(245./255., 0, 0, 1);
+    vec4 orange = vec4(255., 140, 0, 1);
     vec4 color = mix(black, orange, mixer);;
     
     gl_FragColor = color;
